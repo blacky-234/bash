15b81614c39dfddd6a0121e9240b02c4a9c2075c
blacky <blacky@blacky.hackers>
2025-08-15 12:43:36 +0530

    docker container Name

--- dockercommand/containerip.sh
+++ dockercommand/containerip.sh
@@ -8,11 +8,10 @@ for container in $(docker ps -q); do
 
     echo "Container ID: $container"
     echo "Container IP: $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$container")"
-    echo "Container Name: "
+    echo "Container Name: $CONTAINER_NAME"
     echo "Container Status: $(docker inspect -f '{{.State.Status}}' "$container")"
     echo "Container Image: $(docker inspect -f '{{.Config.Image}}' "$container")"
     echo "Container Ports: $(docker inspect -f '{{range .NetworkSettings.Ports}}{{.}}{{end}}' "$container")"
     echo "Container Port Mappings: $(docker port "$container")"
     echo "========================================================================================="
-
 done
